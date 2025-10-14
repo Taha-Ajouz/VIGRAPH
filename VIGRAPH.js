eae528f6ba9ef1a0fe9951950f79ba8285d50ef8
Taha Ajouz <tahaajouz389@gmail.com>
2025-10-14 18:44:15 +0300

    Uploading My VIGRAPH Website

---
+++ VIGRAPH.js
@@ -0,0 +1,38 @@
+let nameInput = document.getElementById('name');
+let subjectInput = document.getElementById('subject');
+let emailInput = document.getElementById('email');
+let messageInput = document.getElementById('message');
+let saveBtn = document.getElementById('saveBtn');
+
+saveBtn.addEventListener('click', () => {
+  if (
+    nameInput.value === '' ||
+    emailInput.value === '' ||
+    subjectInput.value === '' ||
+    messageInput.value === ''
+  ) {
+    document.getElementById('messagebox').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+    ok.addEventListener('click', () => {
+      document.getElementById('messagebox').style.display = 'none';
+      document.getElementById('overlay').style.display = 'none';
+    });
+  } else {
+    localStorage.setItem('Name', nameInput.value);
+    localStorage.setItem('Email', emailInput.value);
+    localStorage.setItem('Subject', subjectInput.value);
+    localStorage.setItem('Message', messageInput.value);
+
+    document.getElementById('messagebox').style.display = 'block';
+    document.getElementById('welc').textContent = "You're Welcome !";
+    document.getElementById('overlay').style.display = 'block';
+    ok.addEventListener('click', () => {
+      document.getElementById('messagebox').style.display = 'none';
+      document.getElementById('overlay').style.display = 'none';
+    });
+    nameInput.value = '';
+    emailInput.value = '';
+    subjectInput.value = '';
+    messageInput.value = '';
+  }
+});
